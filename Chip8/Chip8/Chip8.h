3541c7b9da198c55bf62ede983b1a7f2bf843776
shikhindahikar <shikhindah@gmail.com>
2020-07-03 14:51:43 +0530

    Fetch decode execute loop + main func
    Referred the original code a little bit. Not exactly same as orginal code  but I think it will work.

--- Chip8/Chip8/Chip8.h
+++ Chip8/Chip8/Chip8.h
@@ -19,12 +19,16 @@ class Chip8
 		uint32_t video[64 * 32]{};
 		uint16_t opcode;
 
+		void LoadROM(char const* filename);
+		void Cycle();
 
+		//function declaration
 
+		void Table0();
+		void Table8();
+		void TableE();
+		void TableF();
 
-		Chip8();
-
-		void LoadROM(char const* filename);		//function declaration
 		void OP_NULL();
 		void OP_00E0(); 
 		void OP_00EE();
@@ -73,9 +77,63 @@ Chip8::Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().cou
 	// Initialize RNG
 	randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
 
+	// Set up function pointer table
+	table[0x0] = &Chip8::Table0;
+	table[0x1] = &Chip8::OP_1nnn;
+	table[0x2] = &Chip8::OP_2nnn;
+	table[0x3] = &Chip8::OP_3xkk;
+	table[0x4] = &Chip8::OP_4xkk;
+	table[0x5] = &Chip8::OP_5xy0;
+	table[0x6] = &Chip8::OP_6xkk;
+	table[0x7] = &Chip8::OP_7xkk;
+	table[0x8] = &Chip8::Table8;
+	table[0x9] = &Chip8::OP_9xy0;
+	table[0xA] = &Chip8::OP_Annn;
+	table[0xB] = &Chip8::OP_Bnnn;
+	table[0xC] = &Chip8::OP_Cxkk;
+	table[0xD] = &Chip8::OP_Dxyn;
+	table[0xE] = &Chip8::TableE;
+	table[0xF] = &Chip8::TableF;
+
+	table0[0x0] = &Chip8::OP_00E0;
+	table0[0xE] = &Chip8::OP_00EE;
+
+	table8[0x0] = &Chip8::OP_8xy0;
+	table8[0x1] = &Chip8::OP_8xy1;
+	table8[0x2] = &Chip8::OP_8xy2;
+	table8[0x3] = &Chip8::OP_8xy3;
+	table8[0x4] = &Chip8::OP_8xy4;
+	table8[0x5] = &Chip8::OP_8xy5;
+	table8[0x6] = &Chip8::OP_8xy6;
+	table8[0x7] = &Chip8::OP_8xy7;
+	table8[0xE] = &Chip8::OP_8xyE;
+
+	tableE[0x1] = &Chip8::OP_ExA1;
+	tableE[0xE] = &Chip8::OP_Ex9E;
+
+	tableF[0x07] = &Chip8::OP_Fx07;
+	tableF[0x0A] = &Chip8::OP_Fx0A;
+	tableF[0x15] = &Chip8::OP_Fx15;
+	tableF[0x18] = &Chip8::OP_Fx18;
+	tableF[0x1E] = &Chip8::OP_Fx1E;
+	tableF[0x29] = &Chip8::OP_Fx29;
+	tableF[0x33] = &Chip8::OP_Fx33;
+	tableF[0x55] = &Chip8::OP_Fx55;
+	tableF[0x65] = &Chip8::OP_Fx65;
+
+	
+
+	typedef void (Chip8::*Chip8Func)();
+	Chip8Func table[0xF + 1]{ &Chip8::OP_NULL };
+	Chip8Func table0[0xE + 1]{ &Chip8::OP_NULL };
+	Chip8Func table8[0xE + 1]{ &Chip8::OP_NULL };
+	Chip8Func tableE[0xE + 1]{ &Chip8::OP_NULL };
+	Chip8Func tableF[0x65 + 1]{ &Chip8::OP_NULL };
+
+
 }
 
-void Chip8::LoadROM(char const* filename)			//function definition
+void Chip8::LoadROM(char const* filename)			
 {
 	// Open the file as a stream of binary and move the file pointer to the end
 	std::ifstream file(filename, std::ios::binary | std::ios::ate);
@@ -121,6 +179,50 @@ uint8_t fontset[FONTSET_SIZE] =
 	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
 	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
+
+void Chip8::Cycle()
+{
+	// Fetch
+	opcode = (memory[pc] << 8u) | memory[pc + 1];
+
+	// Increment the PC before we execute anything
+	pc += 2;
+
+	// Decode and Execute
+	((*this).*(table[(opcode & 0xF000u) >> 12u]))();
+
+	// Decrement the delay timer if it's been set
+	if (delayTimer > 0)
+	{
+		--delayTimer;
+	}
+
+	// Decrement the sound timer if it's been set
+	if (soundTimer > 0)
+	{
+		--soundTimer;
+	}
+}
+
+void Chip8::Table0()
+{
+	((*this).*(table0[opcode & 0x000Fu]))();
+}
+
+void Chip8::Table8()
+{
+	((*this).*(table8[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableE()
+{
+	((*this).*(tableE[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableF()
+{
+	((*this).*(tableF[opcode & 0x00FFu]))();
+}
 
 void Chip8::OP_NULL() //Do nothing
 {}
